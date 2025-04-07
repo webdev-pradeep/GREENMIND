eff1280601b1e576bfc87880389706f4d017b6c8
Pradeep <webdevpradeep0101@gmail.com>
2025-04-07 05:47:13 -1200

    kam kiya gaya hai

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BsSearch } from "react-icons/bs";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { GiTreehouse } from "react-icons/gi";
+import { SlCallOut } from "react-icons/sl";
+import { PiShippingContainerFill } from "react-icons/pi";
 
 function HomePage() {
   return (
@@ -114,6 +117,98 @@ function HomePage() {
             </p>
             <p className="text-xl pl-7 pb-5">₱ 3,500.00</p>
           </ul>
+        </div>
+
+        <h1 className="md:text-4xl md:text-black md:font-bold md:font-serif md:text-center md:mt-9">
+          About us
+        </h1>
+        <p className="md:text-xl md:text-black md:font-serif md:text-center">
+          Order now and appreciate the beauty of nature
+        </p>
+
+        <ul className="md:flex md:justify-center md:items-center md:gap-5 md:mt-12 ">
+          <ul className="md:h-[250px] bg-white rounded-2xl  md:p-5 md:w-[400px] md:hover:shadow-2xl md:transition-all md:duration-500  md:hover:transform hover:translate-2">
+            <li className="md:h-18  md:w-18 rounded-full bg-[#C1DCDC] md:flex md:items-center md:justify-center md:hover:bg-red-200 md:transition-all md:duration-500 md:active:bg-red-100 ml-[150px]">
+              <GiTreehouse className="md:text-5xl md:cursor-pointer " />
+            </li>
+            <li className="md:text-2xl md:font-black md:font-serif md:text-center md:mt-6 ">
+              Large Assortment
+            </li>
+            <li className="md:text-ms text-center">
+              we offer many different types of products with fewer variations in
+              each category.
+            </li>
+          </ul>
+
+          <ul className="md:h-[250px] bg-white rounded-2xl  md:p-5 md:w-[400px] md:hover:shadow-2xl md:transition-all md:duration-500 md:hover:translate-1">
+            <li className="md:h-18  md:w-18 rounded-full bg-[#C1DCDC] md:flex md:items-center md:justify-center md:hover:bg-red-200 md:transition-all md:duration-500 md:active:bg-red-100 ml-[150px] ">
+              <SlCallOut className="md:text-4xl md:cursor-pointer" />
+            </li>
+            <li className="md:text-2xl md:font-black md:font-serif md:text-center md:mt-5 ">
+              Fast & Free Shipping
+            </li>
+            <li className="md:text-ms text-center">
+              4-day or less delivery time, free shipping and an expedited
+              delivery option.
+            </li>
+          </ul>
+
+          <ul className="md:h-[250px] bg-white rounded-2xl  md:p-5 md:w-[400px] md:hover:shadow-2xl md:transition-all md:duration-500 md:hover:translate-1">
+            <li className="md:h-18  md:w-18 rounded-full bg-[#C1DCDC] md:flex md:items-center md:justify-center md:hover:bg-red-200 md:transition-all md:duration-500 md:active:bg-red-100 ml-[150px]">
+              <PiShippingContainerFill className="md:text-4xl md:cursor-pointer" />
+            </li>
+            <li className="md:text-2xl md:font-black md:font-serif md:text-center md:mt-5">
+              24/7 Support
+            </li>
+            <li className="md:text-ms text-center">
+              answers to any business related inquiry 24/7 and in real-time.
+            </li>
+          </ul>
+        </ul>
+
+        <h1 className="md:text-4xl md:text-black md:font-bold md:font-serif md:text-center md:mt-9">
+          Categories
+        </h1>
+        <p className="md:text-xl md:text-black md:font-serif md:text-center">
+          Find what you are looking for{" "}
+        </p>
+
+        <div className="md:h-[480px] md:w-auto bg-[#C1DCDC] md:flex  md:justify-center md:items-center mt-8 rounded-2xl relative md:shadow-2xl">
+          <div className="w-auto relative right-50">
+            <img
+              src="src/assets/image 5.png"
+              alt="image5"
+              className="md:w-[250px] object-contain cursor-pointer md:transition-all md:duration-500 md:ease-in-out hover:md:shadow-2xl hover:shadow-black md:rounded-xl hover:md:translate-1"
+            />
+            <a href="#" className="text-xl font-bold flex justify-center mt-5">
+              Natural Plants
+            </a>
+          </div>
+
+          <div className="w-auto absolute top-0">
+            <img
+              src="src/assets/image 6.png"
+              alt="image6"
+              className="md:w-[250px] object-contain cursor-pointer md:transition-all md:duration-500 md:ease-in-out hover:md:shadow-2xl hover:shadow-black md:rounded-xl hover:md:translate-1"
+            />
+            <a href="#" className="text-xl font-bold flex justify-center mt-5">
+              Plant Accessories
+            </a>
+            <p className="text-[10px] text-center text-black font-serif">
+              Horem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+
+          <div className="w-auto md:relative left-50">
+            <img
+              src="src/assets/image 7.png"
+              alt="image7"
+              className="md:w-[250px] object-contain cursor-pointer md:transition-all md:duration-500 md:ease-in-out hover:md:shadow-2xl hover:shadow-black md:rounded-xl hover:md:translate-1 "
+            />
+            <a href="#" className="text-xl font-bold flex justify-center mt-4 ">
+              Artificial Plants
+            </a>
+          </div>
         </div>
       </div>
     </>
